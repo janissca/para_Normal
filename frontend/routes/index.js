372178e8df1d868183e66f7317f2b917c50ebe03
higaharr <higaharr@higaharrs-iMac.local>
2024-09-20 18:19:10 +0700

    add display of fake data

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
+var obj;
+
+fetch('https://jsonplaceholder.typicode.com/posts/')
+	.then(res => res.json())
+	.then(data => {
+		obj = data;
+	})
+	.then(() => {
+		console.log(obj);
+	});
+
 app.get('/', function (req, res) {
-	res.render('index', { title: 'Крона - Мероприятия' })
+
+	res.render('index', { title: 'Крона - Мероприятия', data: obj })
 });
 
 
